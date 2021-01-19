acb5179164d19be20028da9a66b7de9cd786847b
Salvador AG <salvadoraciengomez@gmail.com>
2021-01-19 08:13:14 +0100

    Init

---
+++ 1.php
@@ -0,0 +1,17 @@
+<?php
+
+    class Book {
+        public $isbn;
+        public $title;
+        public $author;
+        public $available;
+    }
+
+    class Customer{
+
+    }
+    
+
+    $book = new Book();
+
+?>

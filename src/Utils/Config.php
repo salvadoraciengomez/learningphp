42347881ab280f3034629fd7ff2c342ee44c6fdb
Salvador AG <salvadoraciengomez@protonmail.com>
2021-01-20 12:21:26 +0100

    Singleton Solved

--- src/Utils/Config.php
+++ src/Utils/Config.php
@@ -11,20 +11,21 @@
         private static $instance;
 
         private function __construct(){
-            $json = file_get_contents(__DIR__ . '/../../config/app.json');
+            $json = file_get_contents(__DIR__ .'/../../config/app.json');
             $this -> data = json_decode($json, true);
             //self::$data = json_decode($json, true);
         }
 
         public static function getInstance():Config{
-            if (self::$instance == null) {self::$instance = new Config();}
+            if (self::$instance == null) {self::$instance = new Self();}
             return self::$instance;
         }
         public function get(string $key){
-            if(!isset($this->$data[$key])){
+            var_dump($this->data);
+            if(!isset($this->data[$key])){
                 throw new NotFoundException("Key $key not in config.");
             }
-            return $this->$data[$key];
+            return $this->data[$key];
         }
     }
 ?>

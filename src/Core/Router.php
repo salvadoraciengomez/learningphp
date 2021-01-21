084afc23e0244f8065c52af3ee2140b9df7d9031
Salvador AG <salvadoraciengomez@protonmail.com>
2021-01-21 10:27:25 +0100

    Advanced Router to Controller

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -13,12 +13,12 @@
 
         public function __construct(){
             //Al crear Router se lee el routes.json para comprobar si corresponde a algún controlador el objeto Request enviado al método route()
-            $json = \file_get_contents( __DIR__ . '/../../config/routes.json');
+            $json = file_get_contents( __DIR__ . '/../../config/routes.json');
             $this->routeMap= json_decode($json, true);
         }
 
         public function route(Request $request): string{
-            //coge un objeto Request y devuelve una cadena que se recibirá al cliente. Comprueba todas las rutas hasta encontrar la propia y enviar al controlador
+            //coge un objeto Request y devuelve una cadena que recibirá el cliente. Comprueba todas las rutas hasta encontrar la propia y enviarla al controlador
             $path = $request->getPath();
             foreach ($this->routeMap as $route => $info){
                 $regexRoute = $this->getRegexRoute($route, $info);
@@ -58,8 +58,62 @@
                     );
                 }
             }
-
-            return $route;
+           return $route;
         }
+
+        private function extractParams(
+            //Para ejecutar el controlador se necesitan:
+            //1) El nombre de la clase a instanciar (dentro de $info)
+            //2) El nombre del método a ejecutar (dentro de $info)
+            //3) Los parámetros para el método
+
+            string $route,
+            string $path
+        ):array {
+            $params = [];
+
+            $pathParts= explode('/', $path);
+            $routeParts= explode('/', $route);
+            //Se espera que la ruta de la petición y la URL de la ruta sigan el mismo patrón
+            //Con explode se obtienen dos arrays $pathParts $routeParts que deben concordar en cada una de sus entradas. 
+            //Se iteran y por cada entrada de $route que sea como un parámetro se mete en la URL
+            //Ejemplo: si tiene la ruta '/books/:id/borrow' y path '/books/12/borrow' el resultado sería el array ['id'=>12]
+
+            foreach ($routeParts as $key => $routePart){
+                if (strpos($routePart, ':')===0){
+                    $name= substr($routePart,1);
+                    $params[$name]=$pathParts[$key+1];
+                }
+            }
+            return $params;
+        }
+
+        private function executeController(
+            //Ejecuta el controlador para una ruta dada
+            //Ya se tiene el nombre de la clase, el método y los argumentos y se usará call_user_func_array
+
+
+            string $route,
+            string $path,
+            array $info,
+            Request $request
+        ): string{
+            $controllerName= '\Bookstore\Controllers\\'.$info['controller'].'Controller';
+            $controller= new $controllerName($request);
+
+            if(isset($info['login']) && $info['login']){
+                if($request->getCookies()->has('user')){
+                    $customerId= $request->getCookies()->get('user');
+                    $controller->setCustomerId($customerId);
+                }else{
+                $errorController= new CustomerController($request);
+                //Si el usuario no tiene una cookie se ejecuta el método login
+                return $errorController->login();
+                }
+            }
+            $params = $this->extractParams($route, $path);
+            return \call_user_func_array([$controller, $info['method']],$params);
+        }
+        
     }
 ?>

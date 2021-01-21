5bea44320e80bcf7945a8f8ebc4ebdccbd48f4ea
Salvador AG <salvadoraciengomez@protonmail.com>
2021-01-21 10:42:37 +0100

    DB Model added

---
+++ src/Models/AbstractModel.php
@@ -0,0 +1,13 @@
+<?php
+    namespace Bookstore\Models;
+
+    use PDO;
+
+    abstract class AbstractModel{
+        private $db;
+
+        public function __construct(PDO $db){
+            $this->db = $db;
+        }
+    }
+?>

3f80406daabc7837b3bde2e0b5a931982a2ded3d
Salvador AG <salvadoraciengomez@protonmail.com>
2021-01-21 13:14:38 +0100

    FAIL:not found twigloader

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
-
-    require_once __DIR__ . '/vendor/autoload.php';
-    $loader = new Twig_Loader_Filesystem(__DIR__ . '/views');
+    var_dump(__DIR__);
+    require_once __DIR__ . '/../vendor/autoload.php';
+    $loader = new Twig_Loader_Filesystem(__DIR__ . '/../views');
     $twig = new Twig_Environment($loader);
 
     $bookModel = new BookModel(Db::getInstance());

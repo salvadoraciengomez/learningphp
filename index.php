48cd7a88df7bf4ea998051ebc0692f134439e74f
Salvador AG <salvadoraciengomez@protonmail.com>
2021-01-22 08:51:31 +0100

    Trying to solve Twig BookModel Db

--- index.php
+++ index.php
@@ -6,15 +6,24 @@
     use Bookstore\Utils\Config;
     use Bookstore\Core\Db;
 
-
+    //OLD TWIG INSTNC
     //$loader = new Twig_Loader_Filesystem(__DIR__ . '/views');
     //$twig = new Twig_Environment($loader);
+
+    //** TRING TO SOLVE DB NULL
+    // $config = new Config();
+
+    // $dbConfig = $config->get('db');
+    // $db = new PDO(
+    //     'mysql:host=127.0.0.1;dbname=bookstore',
+    //     $dbConfig['user'],
+    //     $dbConfig['password']
+    // );*/
 
 
     $loader= new \Twig\Loader\FilesystemLoader(__DIR__.'/views');
     $twig = new \Twig\Environment($loader);
     
-
     $bookModel = new BookModel(Db::getInstance());
     $book = $bookModel->get(1);
 

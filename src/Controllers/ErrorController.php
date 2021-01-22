f9281dfff3caa477c361dc6d65d0093f318d9b64
Salvador AG <salvadoraciengomez@protonmail.com>
2021-01-22 13:25:11 +0100

    error controller added

---
+++ src/Controllers/ErrorController.php
@@ -0,0 +1,14 @@
+<?php
+    namespace Bookstore\Controllers;
+
+    class ErrorController extends AbstractController{
+        
+        public function notFound(): string{
+
+            $properties = ['errorMessage'=>'Page not found!'];
+
+            return $this->render('error.twig', $properties);
+        }
+
+    }
+?>

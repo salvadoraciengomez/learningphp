5d1d5fd402845de96b9ec911c2c2c6eef9171803
Salvador AG <salvadoraciengomez@protonmail.com>
2021-01-25 10:15:05 +0100

    Removed Singletons, implemented DependencyInjector

--- index.php
+++ index.php
@@ -43,6 +43,32 @@
     echo $twig->loadTemplate('sale.twig')->render($params);
     */
 
+
+    //Adding Dependency Injector
+    $config = new Config();
+
+    $dbConfig = $config->get('db');
+
+    $db = new PDO(
+        'mysql:host=127.0.0.1;dbname=bookstore',
+        $dbConfig['user'],
+        $dbConfig['password']
+    );
+
+    $loader = new Twig_Loader_Filesystem(__DIR__.'/../../views');
+    $view = new Twig_Environment($loader);
+
+    $log = new Logger('bookstore');
+    $logFile = $config->get('log');
+    $log->pushHandler(new StreamHandler($logFile, Logger::DEBUG));
+
+    $di = new DependencyInjector();
+    $di->set('PDO', $db);
+    $di->set('Utils\Config', $config);
+    $di->set('Twig_Environment', $view);
+    $di->set('Logger', $log);
+
+
     $router = new Router();
     $response = $router->route(new Request());
     echo $response;
--- src/Core/Config.php
+++ src/Core/Config.php
@@ -8,6 +8,7 @@
     class Config{
 
         private $data;
+        /** OLD SINGLETON REPLACED BY DEPENDENCYINJECTOR (also replacing in Core\Db)
         private static $instance;
 
         private function __construct(){
@@ -27,6 +28,16 @@
             }
             return $this->data[$key];
 
+        }*/
+
+        public function __construct(){
+            $json = \file_get_contents(__DIR__.'/../../config/app.json');
+            $this->data = json_decode($json, true);
+        }
+
+        public function get ($key){
+            if (!isset($this->data[$key])) throw new NotFoundException("Key $key not in config.");
+            return $this->data[$key];
         }
     }
 ?>

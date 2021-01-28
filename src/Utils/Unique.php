59655c349dc92038dd5bde274b6e93ceae3c86d8
Salvador AG <salvadoraciengomez@protonmail.com>
2021-01-28 10:06:56 +0100

    Prepairing Unique trait to PHPUnit debugging

--- src/Utils/Unique.php
+++ src/Utils/Unique.php
@@ -16,27 +16,29 @@
 
         public function setId(int $id=null){
 
-            if ($id < 0){
-                //throw new \Exception('Id cannot be negative'); //Necesita 'use Exception;' si no se utiliza el backslash
-                throw new InvalidException('Id cannot be negative');
-            }
-            if (empty($id)){
-                $this->id= ++self::$lastId;
-            }else{
-                $this->id = $id;
-                if ($id > self::$lastId){
-                    self::$lastId = $id;
-                }
-            }
+            // if ($id < 0){
+            //     //throw new \Exception('Id cannot be negative'); //Necesita 'use Exception;' si no se utiliza el backslash
+            //     throw new InvalidException('Id cannot be negative');
+            // }
+            // if (empty($id)){
+            //     $this->id= ++self::$lastId;
+            // }else{
+            //     $this->id = $id;
+            //     if ($id > self::$lastId){
+            //         self::$lastId = $id;
+            //     }
+            // }
             
-            if ($this->id >50){
-                throw new ExceededMaxAllowedException('Max number of users is 50');
-            }
+            // if ($this->id >50){
+            //     throw new ExceededMaxAllowedException('Max number of users is 50');
+            // }
+
+            $this->id = $id;
         }
 
-        public static function getLastId(): int{
-            return self::$lastId;
-        }
+        // public static function getLastId(): int{
+        //     return self::$lastId;
+        // }
 
         public function getId():int{
             return $this->id;

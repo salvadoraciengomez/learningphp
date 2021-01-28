97fab11fb5e1abadae2f9cbf419fccc0694cc7b9
Salvador AG <salvadoraciengomez@protonmail.com>
2021-01-28 10:16:12 +0100

    Updated first test

--- tests/Domain/Customer/BasicTest.php
+++ tests/Domain/Customer/BasicTest.php
@@ -5,13 +5,24 @@
     use PHPUnit_Framework_TestCase;
 
     class BasicTest extends PHPUnit_Framework_TestCase{
-        public function testAmountToBorrow(){
+        
+        public function testAmountToBorrow(){ //Los métodos que empiezan por "test" son los que ejecuta PHPUnit
+            
             $customer = new Basic (1, 'han', 'solo', 'han@solo.com');
+            
+            //Para comprueba los dos primeros parámetros
             $this->assertSame(
                 3,
                 $customer->getAmountToBorrow(),
-                'Basic customer should borrow up to 3 books.'
+                'Basic customer should borrow up to 3 books.'//Lo muestra en caso de fallo
             );
         }
     }
+
+/**
+ *  @test
+ */
+//public function thisIsATestToo(){}
+//Convierte nombres de métodos que no empiecen por test en funciones de comprobación PHPUnit
+
 ?>

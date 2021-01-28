1d6a639df59dc54749a77325e5ac07e534efdc30
Salvador AG <salvadoraciengomez@protonmail.com>
2021-01-28 11:12:13 +0100

    assertInstanceOf

---
+++ tests/Domain/Customer/CustomerFactoryTest.php
@@ -0,0 +1,22 @@
+<?php
+    namespace Bookstore\Test\Domain\Customer;
+
+    use Bookstore\Domain\Customer\CustomerFactory;
+    use PHPUnit_Framework_TestCase;
+
+    class CustomerFactoryTest extends PHPUnit_Framework_TestCase{
+
+        public function testFactoryBasic(){
+            $customer = CustomerFactory::factory(
+                'basic',1,'han','solo','han@solo.com'
+            );
+
+            $this->assertInstanceOf(
+                //comprueba si pertenece a una clase
+                Basic::class,
+                $customer,
+                'basic should create a Customer\Basic object'.
+            );
+        }
+    }
+?>
